cb7cce34a96216c8246ce55e5e7ce090a468b820
luisfbgs <arueluis@gmail.com>
2018-10-31 15:17:43 -0300

    Server bonito

--- run_db.py
+++ run_db.py
@@ -1,3 +1,4 @@
+import datetime
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
@@ -8,9 +9,10 @@ print("Digite o título da notícia:")
 title = input()
 print("Digite a doença relacionada à noticia:")
 desease = input()
+print("Digite o local relacionado à doença:")
+place = input()
 
-collection.insert({"titulo":title, "doenca":desease})
+collection.insert({"titulo" : title, "doença" : desease, "mod_data" : datetime.datetime.now(), "local" : place})
 
-aux = collection.find()
-for item in aux:
+for item in collection.find():
 	print(item)
