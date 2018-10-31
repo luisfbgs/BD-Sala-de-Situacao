9ab1ad19a796f62142ae7d3bc87d8a1b1a5dd9a2
luisfbgs <arueluis@gmail.com>
2018-10-31 16:32:42 -0300

    Retrieve mais bonito

--- server/main.py
+++ server/main.py
@@ -7,28 +7,29 @@ client = MongoClient('localhost', 27017)
 database = client.sala_db
 collection = database.news
 
-def retrieve(place = "", title = "", desease = "", year = 1, month = 1, day = 1, hour = 0):
+db_api = Flask(__name__)
+
+def retrieve_query(place = "", title = "", desease = "", year = 1, month = 1, day = 1, hour = 0):
 	return collection.find({"local" : {"$regex" : "^" + place},
-								"titulo" : {"$regex" : "^" + title},
-								"doença" : {"$regex" : "^" + desease},
-								"mod_data" : {"$gte" : datetime.datetime(year, month, day, hour)}})
+			"titulo" : {"$regex" : "^" + title},
+			"doença" : {"$regex" : "^" + desease},
+			"mod_data" : {"$gte" : datetime.datetime(year, month, day, hour)}})
 
 
 db_api = Flask(__name__)
 
 @db_api.route('/retrieve', methods = ['GET'])
-def teste():
-	place_arg = request.args.get('place', "")
-	title_arg= request.args.get('title', "")
-	desease_arg = request.args.get('desease', "")
-	year_arg = request.args.get('year', 1)
-	month_arg = request.args.get('month', 1)
-	day_arg = request.args.get('day', 1)
-	hour_arg = request.args.get('hour', 0)
-	return ''.join([str(item) for item in retrieve(place = place_arg, title = title_arg, desease = desease_arg, year = year_arg, month = month_arg, day = day_arg, hour = hour_arg)])
+def retrieve():
+	place = request.args.get('place', "")
+	title = request.args.get('title', "")
+	desease = request.args.get('desease', "")
+	year = request.args.get('year', 1)
+	month = request.args.get('month', 1)
+	day = request.args.get('day', 1)
+	hour = request.args.get('hour', 0)
+	return ''.join([str(item) for item in retrieve_query(place, title, desease, year, month, day, hour)])
+
 
 if __name__ == "__main__":
-	for item in retrieve():
-		print(item)
 	port = int(os.environ.get('PORT', 5000))
 	db_api.run(host = '0.0.0.0', port = port)
