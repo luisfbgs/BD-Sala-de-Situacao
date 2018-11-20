1d3904b3132c6a89b0600d7ed316f1dbeed343e6
luisfbgs <arueluis@gmail.com>
2018-11-20 12:55:29 -0200

    Código refatorado

--- server/main.py
+++ server/main.py
@@ -6,59 +6,70 @@ from bson.json_util import loads
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://adm_sala_st:' + os.environ['DB_PASS'] + os.environ['CLUSTER_U']) 
-database = client.sala_db
-collection = database.news
+CLIENT = MongoClient('mongodb+srv://adm_sala_st:' + os.environ['DB_PASS'] + os.environ['CLUSTER_U'])
+DATABASE = CLIENT.sala_db
+COLLECTION = DATABASE.news
 
-db_api = Flask(__name__)
+DB_API = Flask(__name__)
 
-def retrieve_query(content = "", country = "", region = "", title = "", disease = "", year = 1, month = 1, day = 1, hour = 0):
-	return collection.find({"title" : {"$regex" : "^" + title},
-							"country" : {"$regex" : "^" + country},
-							"content" : {"$regex" : "^" + content},
-							"region" : {"$regex" : "^" + region},
-							"disease" : {"$regex" : "^" + disease},
-							"mod_date" : {"$gte" : datetime.datetime(year, month, day, hour)}})
+def retrieve_query(content="", local=("", ""), title="", disease="", date=(1, 1, 1, 0)):
+    """Requests and returns the list of documents in the database
+     with prefixs that match the arguments"""
+    year, month, day, hour = date
+    country, region = local
+    return COLLECTION.find({"title" : {"$regex" : "^" + title},
+                            "country" : {"$regex" : "^" + country},
+                            "content" : {"$regex" : "^" + content},
+                            "region" : {"$regex" : "^" + region},
+                            "disease" : {"$regex" : "^" + disease},
+                            "mod_date" : {"$gte" : datetime.datetime(year, month, day, hour)}})
 
 def insert_query(json_content):
-	new_id = collection.insert_one(json_content).inserted_id
-	collection.update_one({'_id' : new_id}, {'$set' : {'mod_date' : datetime.datetime.now()}})
-	return new_id
+    """Inserts the json document in the database with a modification date attached.
+    Returns the ID of the new document in the database"""
+    new_id = COLLECTION.insert_one(json_content).inserted_id
+    COLLECTION.update_one({'_id' : new_id}, {'$set' : {'mod_date' : datetime.datetime.now()}})
+    return new_id
 
 def check_input_json(input_json):
-	keys = ['source', 'author', 'title', 'description', 'url', 'url_to_image', 'country', 'region', 'score', 'date', 'disease']
-	for k in keys:
-		assert k in input_json
+    """Checks if the recieved json contains the required fields"""
+    keys = ['source', 'author', 'title', 'description', 'url',
+            'url_to_image', 'country', 'region', 'score', 'date', 'disease']
+    for k in keys:
+        assert k in input_json
 
-@db_api.route('/retrieve', methods = ['GET'])
+@DB_API.route('/retrieve', methods=['GET'])
 def retrieve():
-	country = request.args.get('country', "")
-	region = request.args.get('region', "")
-	title = request.args.get('title', "")
-	content = request.args.get('content', "")
-	disease = request.args.get('disease', "")
-	year = int(request.args.get('year', 1))
-	month = int(request.args.get('month', 1))
-	day = int(request.args.get('day', 1))
-	hour = int(request.args.get('hour', 0))
-	
-	query_str = retrieve_query(content, country, region, title, disease, year, month, day, hour);
-	query_str = dumps(query_str)
-	return jsonify(json.loads(query_str))
- 
-@db_api.route('/insert', methods = ['GET'])
+    """Requests the desired documents from the database"""
+    country = request.args.get('country', "")
+    region = request.args.get('region', "")
+    title = request.args.get('title', "")
+    content = request.args.get('content', "")
+    disease = request.args.get('disease', "")
+    year = int(request.args.get('year', 1))
+    month = int(request.args.get('month', 1))
+    day = int(request.args.get('day', 1))
+    hour = int(request.args.get('hour', 0))
+
+    query_str = retrieve_query(content, (country, region), title, disease, (year, month, day, hour))
+    query_str = dumps(query_str)
+    return jsonify(json.loads(query_str))
+
+@DB_API.route('/insert', methods=['GET'])
 def insert():
-	content = request.args.get('json', "")
-	key = request.args.get('key', "")
-	if key != os.environ['INSERT_KEY']:
-		return "Fail"
-	try:
-		json_content = loads(content)
-		check_input_json(json_content)
-	except:
-		return "Fail"
-	return str(insert_query(json_content))
+    """Inserts the desired document in the database.
+    Returns the document ID if the insertion is successful or 'Fail' otherwise"""
+    content = request.args.get('json', "")
+    key = request.args.get('key', "")
+    if key != os.environ['INSERT_KEY']:
+        return "Fail: incorrect key"
+    try:
+        json_content = loads(content)
+        check_input_json(json_content)
+    except AssertionError as error:
+        return "Fail: " + str(error)
+    return str(insert_query(json_content))
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 5000))
-	db_api.run(host = '0.0.0.0', port = port)
+    PORT = int(os.environ.get('PORT', 5000))
+    DB_API.run(host='0.0.0.0', port=PORT)
